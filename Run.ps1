2299213e484ce7ecc0ff40c23c6ac696798ac5c6
Simon Bjørneseth <simon.bjorneseth@gmail.com>
2024-11-07 23:12:34 +0100

    yes4

--- Run.ps1
+++ Run.ps1
@@ -1,9 +1,9 @@
 $WatchGoalMinutes = 60
 $Streamers = Get-Content -Path "Streamers.json" | ConvertFrom-Json
-while (($Streamers.WatchTime | Get-Unique) -ne $WatchGoalMinutes) {
+while (($Streamers.WatchTimeMinutes | Get-Unique) -ne $WatchGoalMinutes) {
     $Streamers = Get-Content -Path "Streamers.json" | ConvertFrom-Json
     foreach ($Streamer in $Streamers) {
-        if ($Streamer.WatchTime -ge $WatchGoalMinutes) {
+        if ($Streamer.WatchTimeMinutes -ge $WatchGoalMinutes) {
             Write-Host "$($Streamer.Name) has been watched for an hour. Skipping"
             Continue
         }
@@ -11,15 +11,15 @@ while (($Streamers.WatchTime | Get-Unique) -ne $WatchGoalMinutes) {
         if ($Response -match '"isLiveBroadcast":.*true') {
             Write-Host "$($Streamer.Name) is live. Starting to watch"
             start-process chrome.exe "https://www.twitch.tv/$($Streamer.Name)"
-            while ($Streamer.WatchTime -lt $WatchGoalMinutes) {
+            while ($Streamer.WatchTimeMinutes -lt $WatchGoalMinutes) {
                 Start-Sleep -Seconds 1
                 $Response = Invoke-RestMethod -Method Get -Uri "https://www.twitch.tv/$($Streamer.Name)"
                 if ($Response -match '"isLiveBroadcast":.*true') {
-                    Write-Host "$($Streamer.Name) is still live. Saving progress ($($Streamer.WatchTime) minutes) and watching one more minute"
-                    $Streamer.WatchTime = $Streamer.WatchTime + 1
+                    Write-Host "$($Streamer.Name) is still live. Saving progress ($($Streamer.WatchTimeMinutes) minutes) and watching one more minute"
+                    $Streamer.WatchTimeMinutes = $Streamer.WatchTimeMinutes + 1
                     $Progress = Get-Content -Path "Streamers.json" | ConvertFrom-Json
                     # Update progress
-                    $Progress | ForEach-Object {if ($_.Name -eq $Streamer.Name) {[pscustomobject]@{Name = $_.Name;WatchTime = $Streamer.WatchTime}}else{$_}} | ConvertTo-Json | Set-Content -Path "Streamers.json" -Force
+                    $Progress | ForEach-Object {if ($_.Name -eq $Streamer.Name) {[pscustomobject]@{Name = $_.Name;WatchTimeMinutes = $Streamer.WatchTimeMinutes}}else{$_}} | ConvertTo-Json | Set-Content -Path "Streamers.json" -Force
                 }
                 else {
                     Write-Host "$($Streamer.Name) is no longer live"
