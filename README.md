ebda6a47c753fb26471e7d55aaafc8ecd8fa19de
Simon Bjørneseth <simon.bjorneseth@gmail.com>
2024-11-07 23:11:27 +0100

    yes3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Autowatcher
-Will automatically watch streamers based on this list https://twitch.facepunch.com/#drops as of 11/07/2024. Json file can be changed to watch whatever streamer you want.
+Will automatically watch streamers based on this list https://twitch.facepunch.com/#drops as of 11/07/2024. Json file can be changed to watch any streamer. Progress will be saved every 10 minutes.
 
 ## How to use
 Clone repo, right click Run.ps1 and click "Run with PowerShell"
--- Run.ps1
+++ Run.ps1
@@ -3,28 +3,31 @@ $Streamers = Get-Content -Path "Streamers.json" | ConvertFrom-Json
 while (($Streamers.WatchTime | Get-Unique) -ne $WatchGoalMinutes) {
     $Streamers = Get-Content -Path "Streamers.json" | ConvertFrom-Json
     foreach ($Streamer in $Streamers) {
-        if ($Streamer.WatchTime -gt $WatchGoalMinutes) {
+        if ($Streamer.WatchTime -ge $WatchGoalMinutes) {
             Write-Host "$($Streamer.Name) has been watched for an hour. Skipping"
             Continue
         }
         $Response = Invoke-RestMethod -Method Get -Uri "https://www.twitch.tv/$($Streamer.Name)"
         if ($Response -match '"isLiveBroadcast":.*true') {
             Write-Host "$($Streamer.Name) is live. Starting to watch"
+            start-process chrome.exe "https://www.twitch.tv/$($Streamer.Name)"
             while ($Streamer.WatchTime -lt $WatchGoalMinutes) {
-                Start-Sleep -Seconds 600
+                Start-Sleep -Seconds 1
                 $Response = Invoke-RestMethod -Method Get -Uri "https://www.twitch.tv/$($Streamer.Name)"
                 if ($Response -match '"isLiveBroadcast":.*true') {
-                    Write-Host "$($Streamer.Name) is still live. Saving progress and watching for another 10 minutes"
-                    $Streamer.WatchTime = $Streamer.WatchTime + 10
+                    Write-Host "$($Streamer.Name) is still live. Saving progress ($($Streamer.WatchTime) minutes) and watching one more minute"
+                    $Streamer.WatchTime = $Streamer.WatchTime + 1
                     $Progress = Get-Content -Path "Streamers.json" | ConvertFrom-Json
                     # Update progress
                     $Progress | ForEach-Object {if ($_.Name -eq $Streamer.Name) {[pscustomobject]@{Name = $_.Name;WatchTime = $Streamer.WatchTime}}else{$_}} | ConvertTo-Json | Set-Content -Path "Streamers.json" -Force
                 }
                 else {
                     Write-Host "$($Streamer.Name) is no longer live"
+                    Get-Process -Name Chrome -ErrorAction SilentlyContinue | Stop-Process
                     break
                 }
             }
+            Get-Process -Name Chrome -ErrorAction SilentlyContinue | Stop-Process
             Continue
         }
         else {
@@ -33,3 +36,5 @@ while (($Streamers.WatchTime | Get-Unique) -ne $WatchGoalMinutes) {
         }
     }
 }
+Write-Host "Done!" -ForegroundColor Green
+Read-Host -Prompt "Press Enter to exit"
