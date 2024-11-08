765e01100f768687d373f9e0a85e5485e535f5a1
Simon Bjørneseth <simon.bjorneseth@gmail.com>
2024-11-08 17:52:35 +0100

    Update file paths and names for clarity and usability improvements Update Settings.json for clarity and usability improvements Fix typo in README and improve clarity of usage instructions Update README and scripts for clarity and usability improvements Refactor Run.ps1 to use absolute file paths for Settings.json and Streamers.json

--- Run.ps1
+++ Run.ps1
@@ -1,9 +1,9 @@
 # This file is executed by right clicking and selecting "Run with PowerShell". Will loop until all streamers have been watched for the desired amount of time.
-$Settings = Get-Content -Path ".\Settings.json" | ConvertFrom-Json
+$Settings = Get-Content -Path "$PSScriptRoot\Settings.jsonc" | ConvertFrom-Json
 $ProcessName = $Settings.BrowserFullPath.Split("\")[-1].Split(".")[0]
-$Streamers = Get-Content -Path ".\Streamers.json" | ConvertFrom-Json
+$Streamers = Get-Content -Path "$PSScriptRoot\Streamers.json" | ConvertFrom-Json
 while (($Streamers.WatchTimeMinutes | Get-Unique) -ne $Settings.WatchTimeGoalMinutes) {
-    $Streamers = Get-Content -Path ".\Streamers.json" | ConvertFrom-Json
+    $Streamers = Get-Content -Path "$PSScriptRoot\Streamers.json" | ConvertFrom-Json
     foreach ($Streamer in $Streamers) {
         if ($Streamer.WatchTimeMinutes -ge $Settings.WatchTimeGoalMinutes) {
             Write-Host "$($Streamer.Name) has been watched for $($Settings.WatchTimeGoalMinutes). Skipping"
@@ -17,12 +17,12 @@ while (($Streamers.WatchTimeMinutes | Get-Unique) -ne $Settings.WatchTimeGoalMin
                 Start-Sleep -Seconds ($Settings.UpdateIntervalMinutes * 60)
                 $Response = Invoke-RestMethod -Method Get -Uri "https://www.twitch.tv/$($Streamer.Name)"
                 if ($Response -match '"isLiveBroadcast":.*true') {
-                    $Streamer = Get-Content -Path ".\Streamers.json" | ConvertFrom-Json | Where-Object -FilterScript {$_.Name -eq $Streamer.Name}
+                    $Streamer = Get-Content -Path "$PSScriptRoot\Streamers.json" | ConvertFrom-Json | Where-Object -FilterScript {$_.Name -eq $Streamer.Name}
                     $Streamer.WatchTimeMinutes = $Streamer.WatchTimeMinutes + $Settings.UpdateIntervalMinutes
                     Write-Host "$($Streamer.Name) is still live. Saving progress ($($Streamer.WatchTimeMinutes) minutes) and watching $($Settings.UpdateIntervalMinutes) more minute"
-                    $Progress = Get-Content -Path ".\Streamers.json" | ConvertFrom-Json
+                    $Progress = Get-Content -Path "$PSScriptRoot\Streamers.json" | ConvertFrom-Json
                     # Update progress
-                    $Progress | ForEach-Object {if ($_.Name -eq $Streamer.Name) {[pscustomobject]@{Name = $_.Name;WatchTimeMinutes = $Streamer.WatchTimeMinutes}}else{$_}} | ConvertTo-Json | Set-Content -Path ".\Streamers.json" -Force
+                    $Progress | ForEach-Object {if ($_.Name -eq $Streamer.Name) {[pscustomobject]@{Name = $_.Name;WatchTimeMinutes = $Streamer.WatchTimeMinutes}}else{$_}} | ConvertTo-Json | Set-Content -Path "$PSScriptRoot\Streamers.json" -Force
                 }
                 else {
                     Write-Host "$($Streamer.Name) is no longer live"
